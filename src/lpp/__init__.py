95e14d0ebbf537525d919846ceafb59e0dfb3aa9
Phillip Klawansky <pklawansky@gmail.com>
2026-04-18 18:05:42 +0100

    style: alias _get_version to avoid polluting lpp public namespace

--- src/lpp/__init__.py
+++ src/lpp/__init__.py
@@ -1,7 +1,7 @@
 # src/lpp/__init__.py
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import version as _get_version, PackageNotFoundError
 try:
-    __version__ = version("lpp")
+    __version__ = _get_version("lpp")
 except PackageNotFoundError:
     __version__ = "0.0.0+dev"
 
